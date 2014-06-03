4d1a98daf0ee2bc9f6fac2c2fef0dc2799923ef8
Ilya Shakhat <ishakhat@mirantis.com>
2014-06-03 18:57:34 +0400

    Improved utility to dump and restore memcached data
    Utility is able to dump all key-value pairs from memcached. Connection
    parameters are provided vi config file. Also utility has the following
    params:
      --file FILE, -f FILE  File name where to store data
      --restore, -r         Restore data into memcached
      --min-compress-len MIN_COMPRESS_LEN, -m MIN_COMPRESS_LEN
                            The threshold length to kick in auto-compression
    
    Change-Id: Ied6bd2b95cccba42be5a69e13812b2206a0c73b9

--- stackalytics/processor/dump.py
+++ stackalytics/processor/dump.py
@@ -13,10 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import memcache
 import pickle
+import six
 import sys
 
 from oslo.config import cfg
+import re
 
 from stackalytics.openstack.common import log as logging
 from stackalytics.processor import config
@@ -26,12 +29,15 @@ from stackalytics.processor import runtime_storage
 LOG = logging.getLogger(__name__)
 
 OPTS = [
-    cfg.BoolOpt('reverse',
+    cfg.BoolOpt('restore',
                 short='r',
-                help='Load data to runtime storage'),
+                help='Restore data into memcached'),
     cfg.StrOpt('file',
                short='f',
-               help='File name'),
+               help='File name where to store data'),
+    cfg.StrOpt('min-compress-len', default=0,
+               short='m',
+               help='The threshold length to kick in auto-compression'),
 ]
 
 
@@ -44,31 +50,53 @@ def read_records_from_fd(fd):
         yield record
 
 
-def import_data(runtime_storage_inst, fd):
+def store_bucket(memcached_inst, bucket):
+    res = memcached_inst.set_multi(bucket,
+                                   min_compress_len=cfg.CONF.min_compress_len)
+    if res:
+        LOG.critical('Failed to set values in memcached: %s', res)
+        raise Exception('memcached set_multi operation is failed')
+
+
+def import_data(memcached_inst, fd):
     bucket = {}
     count = 0
-    for record in read_records_from_fd(fd):
+    for key, value in read_records_from_fd(fd):
         count += 1
-        if len(bucket) < runtime_storage.RECORD_ID_PREFIX:
-            bucket[record['record_id']] = record
+        if len(bucket) < runtime_storage.BULK_READ_SIZE:
+            bucket[key] = value
         else:
-            if not runtime_storage_inst.memcached.set_multi(
-                    bucket, key_prefix=runtime_storage.RECORD_ID_PREFIX):
-                LOG.critical('Failed to set_multi in memcached')
-                raise Exception('Failed to set_multi in memcached')
+            store_bucket(memcached_inst, bucket)
             bucket = {}
     if bucket:
-        if not runtime_storage_inst.memcached.set_multi(
-                bucket, key_prefix=runtime_storage.RECORD_ID_PREFIX):
-            LOG.critical('Failed to set_multi in memcached')
-            raise Exception('Failed to set_multi in memcached')
-
-    runtime_storage_inst._set_record_count(count)
+        store_bucket(memcached_inst, bucket)
 
 
-def export_data(runtime_storage_inst, fd):
-    for record in runtime_storage_inst.get_all_records():
-        pickle.dump(record, fd)
+def export_data(memcached_inst, fd):
+    slabs = memcached_inst.get_slabs()
+    for slab_number, slab in slabs[0][1].iteritems():
+        count = int(slab['number'])
+        keys = memcached_inst.get_stats(
+            'cachedump %s %s' % (slab_number, count))[0][1].keys()
+
+        n = 0
+        while n < count:
+            LOG.debug('Dumping slab %s, start record %s', slab_number, n)
+
+            for k, v in six.iteritems(memcached_inst.get_multi(
+                    keys[n: min(count, n + runtime_storage.BULK_READ_SIZE)])):
+                pickle.dump((k, v), fd)
+
+            n += runtime_storage.BULK_READ_SIZE
+
+
+def _connect_to_memcached(uri):
+    stripped = re.sub(runtime_storage.MEMCACHED_URI_PREFIX, '', uri)
+    if stripped:
+        storage_uri = stripped.split(',')
+        return memcache.Client(storage_uri)
+    else:
+        raise Exception('Invalid storage uri %s' % uri)
 
 
 def main():
@@ -83,23 +111,22 @@ def main():
     logging.setup('stackalytics')
     LOG.info('Logging enabled')
 
-    runtime_storage_inst = runtime_storage.get_runtime_storage(
-        cfg.CONF.runtime_storage_uri)
+    memcached_inst = _connect_to_memcached(cfg.CONF.runtime_storage_uri)
 
     filename = cfg.CONF.file
 
-    if cfg.CONF.reverse:
+    if cfg.CONF.restore:
         if filename:
             fd = open(filename, 'r')
         else:
             fd = sys.stdin
-        import_data(runtime_storage_inst, fd)
+        import_data(memcached_inst, fd)
     else:
         if filename:
             fd = open(filename, 'w')
         else:
             fd = sys.stdout
-        export_data(runtime_storage_inst, fd)
+        export_data(memcached_inst, fd)
 
 
 if __name__ == '__main__':
