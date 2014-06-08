3781cc85e82cab3d79d78d44668b69897b0f6a31
Monty Taylor <mordred@inaugust.com>
2014-06-08 11:38:57 -0700

    Update to use sensible defaults for oslo.config
    CONF should be passed the name of the application, which means
    that /etc/stackalytics/stackalytics.conf will be what is looked for
    in the absence of other options. This is great, since it's easy
    to put a file in the place that defaults expect.
    
    Change-Id: I11ff134da24931affadc58ad0989185ae6df7e92

--- dashboard/web.py
+++ dashboard/web.py
@@ -47,13 +47,7 @@ LOG = logging.getLogger(__name__)
 conf = cfg.CONF
 conf.register_opts(config.OPTS)
 
-conf_file = os.getenv('STACKALYTICS_CONF')
-if conf_file and os.path.isfile(conf_file):
-    conf(default_config_files=[conf_file])
-    app.config['DEBUG'] = cfg.CONF.debug
-
 logging.setup('dashboard')
-LOG.info('Stackalytics.dashboard is configured via "%s"', conf_file)
 
 
 # Handlers ---------
@@ -602,6 +596,14 @@ def remove_ctrl_chars(text):
 
 
 def main():
+    conf_file = os.getenv('STACKALYTICS_CONF')
+    if conf_file and os.path.isfile(conf_file):
+        conf(default_config_files=[conf_file])
+        app.config['DEBUG'] = cfg.CONF.debug
+        LOG.info('Stackalytics.dashboard is configured via "%s"', conf_file)
+    else:
+        conf(project='stackalytics')
+
     app.run(cfg.CONF.listen_host, cfg.CONF.listen_port)
 
 if __name__ == '__main__':
