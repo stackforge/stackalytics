b8f48953d35779d71613b50ddf0342694f1f09d2
Ken'ichi Ohmichi <ken-oomichi@wx.jp.nec.com>
2017-03-13 13:08:27 -0700

    Add query_launchpad config option
    Launchpad manages many information related to OpenStack at this
    time, but the query takes much time for the processor. In addition,
    stackalytics can be useful for OSS which are not managed with the
    launchpad. So this patch adds a config option query_launchpad so
    that users can skip querying the launchpad. The default option is
    True, so this patch doesn't affect at all on the default option.
    
    Change-Id: I4b7db745c36545e8897132cf6ccbb1c3fd6f3c07

--- stackalytics/config.py
+++ stackalytics/config.py
@@ -55,6 +55,8 @@ PROCESSOR_OPTS = [
                default='https://git.openstack.org/cgit/openstack/i18n/'
                        'plain/tools/zanata/translation_team.yaml',
                help='URI of translation team data'),
+    cfg.BoolOpt("query-launchpad", default=True,
+                help="Query information(users, etc) from launchpad?"),
     cfg.IntOpt('members-look-ahead', default=250,
                help='How many member profiles to look ahead after the last'),
     cfg.IntOpt('read-timeout', default=120,
@@ -83,7 +85,8 @@ DASHBOARD_OPTS = [
                help='Warn if the age of data is more than this value, sec'),
 ]
 
+ALL_OPTS = CONNECTION_OPTS + PROCESSOR_OPTS + DASHBOARD_OPTS
+
 
 def list_opts():
-    yield (None, copy.deepcopy(CONNECTION_OPTS + PROCESSOR_OPTS +
-                               DASHBOARD_OPTS))
+    yield (None, copy.deepcopy(ALL_OPTS))
